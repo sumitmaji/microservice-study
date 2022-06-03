38effe410482418c5001b112523587ac36ae91fc
sumit <skmaji1@outlook.com>
2022-06-03 21:36:27 +0530

    Config server, Eureka

---
+++ appclient-3/licensing-service/src/main/java/com/sum/licenceservice/controllers/LicenseServiceController.java
@@ -0,0 +1,45 @@
+package com.sum.licenceservice.controllers;
+
+import com.sum.licenceservice.model.License;
+import com.sum.licenceservice.services.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @Autowired
+    private LicenseService licenseService;
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public List<License> getLicenses(@PathVariable("organizationId") String organizationId) {
+
+        return licenseService.getLicensesByOrg(organizationId);
+    }
+
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public License getLicenses( @PathVariable("organizationId") String organizationId,
+                                @PathVariable("licenseId") String licenseId) {
+
+        return licenseService.getLicense(organizationId,licenseId);
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
+    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the put");
+    }
+
+    @RequestMapping(value="/",method = RequestMethod.POST)
+    public void saveLicenses(@RequestBody License license) {
+       licenseService.saveLicense(license);
+    }
+
+    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
+        return String.format("This is the Delete");
+    }
+}
