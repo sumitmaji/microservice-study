362216755e6b8b241ec4dc8028e4d77750db9354
sumit <skmaji1@outlook.com>
2022-06-04 10:29:58 +0530

    Config server, Eureka

--- appclient-4/oraganization-service/src/main/resources/data.sql
+++ appclient-4/oraganization-service/src/main/resources/data.sql
@@ -1,13 +1,3 @@
-DROP TABLE IF EXISTS organizations;
-
-CREATE TABLE organizations (
-  organization_id        VARCHAR(100) PRIMARY KEY NOT NULL,
-  name                   TEXT NOT NULL,
-  contact_name           TEXT NOT NULL,
-  contact_email          TEXT NOT NULL,
-  contact_phone          TEXT   NOT NULL);
-
-
 INSERT INTO organizations (organization_id, name, contact_name, contact_email, contact_phone)
 VALUES ('e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'customer-crm-co', 'Mark Balster', 'mark.balster@custcrmco.com', '823-555-1212');
 
