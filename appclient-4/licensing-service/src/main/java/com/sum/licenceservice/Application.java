8108e1096beebeae6e30c4a0a082638c7dc1c9d8
sumit <skmaji1@outlook.com>
2022-06-04 10:07:29 +0530

    Client Resillency

--- appclient-4/licensing-service/src/main/java/com/sum/licenceservice/Application.java
+++ appclient-4/licensing-service/src/main/java/com/sum/licenceservice/Application.java
@@ -1,5 +1,6 @@
 package com.sum.licenceservice;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -22,4 +25,11 @@ public class Application {
   public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
   }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseaServer() throws SQLException {
+        return Server.createTcpServer(
+                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+    }
+
 }
