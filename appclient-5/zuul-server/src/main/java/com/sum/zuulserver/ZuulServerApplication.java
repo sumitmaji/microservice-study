f005dbb79282095203b2ccccb596861e86aaad70
sumit <skmaji1@outlook.com>
2022-06-04 18:00:26 +0530

    Service Discovery with Zuul

--- appclient-5/zuul-server/src/main/java/com/sum/zuulserver/ZuulServerApplication.java
+++ appclient-5/zuul-server/src/main/java/com/sum/zuulserver/ZuulServerApplication.java
@@ -2,11 +2,11 @@ package com.sum.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //Enable service to be a zuul server
-@EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
